6b8c2c7452cd1fc5330aac78ce6019f830401b9a
surya <surya.r@venbainfotech.com>
2023-06-03 23:30:37 +0530

    Task 1- 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,25 +13,54 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message:JSON.stringify(books)});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn
+  let resultData = [];
+  console.log(books)
+  for (let key in books) {  
+      console.log(books[key])
+        if(key === isbn)
+        {
+            resultData.push(books[key])
+        }
+  }
+  return res.status(300).json({message: resultData});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author
+  let resultData = [];
+  for (let key in books) {
+        console.log("books[key]",books[key])
+      for (let book in books[key]) {
+        console.log(book, books[key][book]);
+        if( books[key][book] === author) {
+            resultData.push(books[key])
+        }}}
+  return res.status(300).json({message: resultData});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const fullTitle = req.params.title
+  const title = fullTitle.replace("+", " ")
+  console.log(title)
+  let resultData = [];
+  for (let key in books) {
+      for (let book in books[key]) {
+        console.log(book, books[key][book]);
+        if( books[key][book] === title) {
+            resultData.push(books[key])
+        }}}
+  return res.status(300).json({message: resultData});
 });
 
 //  Get book review
